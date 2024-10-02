1bb695bbbd0a07f8cd4769d74731acd18609dd0a
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2024-10-02 13:17:46 +0800

    add: transaction interface

--- IDataSource.cs
+++ IDataSource.cs
@@ -2,6 +2,9 @@
 {
     public interface IDataSource
     {
+        object? BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
         IQueryable<T> GetQueryable<T>() where T : class;
         IDataWriter<T> GetWriter<T>() where T : class;
         bool Save();
