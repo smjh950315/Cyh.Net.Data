acfc757d907256b96e0e345765b3fe5b5ad0f00f
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2024-10-08 19:27:08 +0800

    add: CompareType comment

--- Predicate/CompareType.cs
+++ Predicate/CompareType.cs
@@ -9,9 +9,25 @@
         GreaterThanOrEqual = 4,
         LessThan = 5,
         LessThanOrEqual = 6,
+
+        /// <summary>
+        /// Target = ?Constant?
+        /// </summary>
         Contains = 7,
+
+        /// <summary>
+        /// Target = ? or ? or ...
+        /// </summary>
         IsAnyOf = 8,
+
+        /// <summary>
+        /// Target = ?Constant_1? or ?Constant_2? or ?Constant_3?...
+        /// </summary>
         ContainsAnyOf = 9,
-        IncludeAnyOf = 10,
+
+        /// <summary>
+        /// ?Target? = Constant_1 or Constant_2 or Constant_3...
+        /// </summary>
+        IncludedByAnyOf = 10,
     }
 }
