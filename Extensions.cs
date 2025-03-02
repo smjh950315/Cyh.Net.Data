3fba1593ea04303add6fd1c873997873b4c425f0
smjh950315 <103402409+smjh950315@users.noreply.github.com>
2025-03-02 17:52:19 +0800

    rebuild: DataRepository

---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Cyh.Net.Data
+{
+    public static class Extensions
+    {
+        public static bool Any<T>(this IDataRepository<T> dataRepository, Expression<Func<T, bool>>? predicate) where T : class
+        {
+            return predicate != null ? dataRepository.Queryable.Any(predicate) : dataRepository.Queryable.Any();
+        }
+        public static IQueryable<T> Where<T>(this IDataRepository<T> dataRepository, Expression<Func<T, bool>>? predicate) where T : class
+        {
+            return predicate != null ? dataRepository.Queryable.Where(predicate) : dataRepository.Queryable;
+        }
+        public static IQueryable<V> Select<T, V>(this IDataRepository<T> dataRepository, Expression<Func<T, V>> selector) where T : class
+        {
+            return dataRepository.Queryable.Select(selector);
+        }
+    }
+}
---
+++ IDataRepository.cs
@@ -0,0 +1,72 @@
+﻿using Cyh.Net.Reflection;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Cyh.Net.Data
+{
+    public interface IDataRepository
+    {
+        IDataRepository<T>? GetRepository<T>() where T : class;
+        int SaveChanges();
+    }
+    public interface IDataRepository<T> : IDataRepository where T : class
+    {
+        IQueryable<T> Queryable { get; }
+        bool Any(Expression<Func<T, bool>> predicate);
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        IQueryable<V> Select<V>(Expression<Func<T, V>> selector);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        void AddRange(IEnumerable<T> entity);
+        void UpdateRange(IEnumerable<T> entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+
+    public static class DataRepositoryExtensions
+    {
+        static Dictionary<Type, MethodInfo> _repositoryFactories = new Dictionary<Type, MethodInfo>();
+        public static object? GetRepository(this IDataRepository repository, Type dataType)
+        {
+            if (_repositoryFactories.TryGetValue(dataType, out MethodInfo? factory))
+            {
+                return factory.Invoke(repository, null);
+            }
+            MethodInfo? method = typeof(IDataRepository).GetMethod(nameof(GetRepository));
+            MethodInfo? genericMethod = method?.MakeGenericMethod(dataType);
+            if (genericMethod == null) return null;
+            _repositoryFactories.Add(dataType, genericMethod);
+            return genericMethod.Invoke(repository, null);
+        }
+        public static void Add(this IDataRepository repository, object entity)
+        {
+            if (entity == null) return;
+            repository.GetRepository(entity.GetType())?.CallMethod("Add", [entity]);
+        }
+        public static void Update(this IDataRepository repository, object entity)
+        {
+            if (entity == null) return;
+            repository.GetRepository(entity.GetType())?.CallMethod("Update", [entity]);
+        }
+        public static void Remove(this IDataRepository repository, object entity)
+        {
+            if (entity == null) return;
+            repository.GetRepository(entity.GetType())?.CallMethod("Remove", [entity]);
+        }
+        public static void AddRange<T>(this IDataRepository repository, IEnumerable<T> entities)
+        {
+            if (entities.IsNullOrEmpty()) return;
+            repository.GetRepository(typeof(T))?.CallMethod("AddRange", [entities]);
+        }
+        public static void UpdateRange<T>(this IDataRepository repository, IEnumerable<T> entities)
+        {
+            if (entities.IsNullOrEmpty()) return;
+            repository.GetRepository(typeof(T))?.CallMethod("UpdateRange", [entities]);
+        }
+        public static void RemoveRange<T>(this IDataRepository repository, IEnumerable<T> entities)
+        {
+            if (entities.IsNullOrEmpty()) return;
+            repository.GetRepository(typeof(T))?.CallMethod("RemoveRange", [entities]);
+        }
+    }
+}
